820c726adb1139c9ef981112c34760c394ceac9c
Erik Toto Rodriguez <eriktotorodriguez@Mac-mini-de-Erik.local>
2023-07-05 21:23:20 -0600

    diseño

--- lib/pages/videos_disponibles/videos_disponibles_page.dart
+++ lib/pages/videos_disponibles/videos_disponibles_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class VideosDisponoblesPage extends StatefulWidget {
   const VideosDisponoblesPage({super.key});
 
@@ -13,295 +12,78 @@ class _VideosDisponoblesPageState extends State<VideosDisponoblesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
-      extendBodyBehindAppBar: true,
+      extendBodyBehindAppBar: false,
       extendBody: true,
       appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.menu),
+        title: Image.asset(
+          'assets/images/logo_aifcf.png',
+          fit: BoxFit.contain,
+          height: 60,
         ),
-        title: Image.asset('assets/images/logo_aifcf.png', fit: BoxFit.contain, height: 60,),
         backgroundColor: Colors.transparent,
         elevation: 0,
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+        // actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
       ),
+      body: _body(),
+    );
+  }
 
-      body: Stack(
+  Widget _body() {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ListView(
-            children: [
-              Container(
-                height: 100,
-                color: Colors.grey,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 1,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      width: 100,
-                      margin: const EdgeInsets.all(8),
-                      color: Colors.red,
-                      child: Center(
-                        child: Text(
-                          'Video $index',
-                          style: const TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    );
-                  },
-                ),
+          // Popular shows section
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              'Todos los cursos disponibles',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 50), // Espacio para dejar lugar a la lista horizontal
-              
-              Container(
-                height: 100,
-                color: Colors.grey,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 100,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      width: 100,
-                      margin: EdgeInsets.all(8),
-                      color: Colors.blue,
-                      child: Center(
-                        child: Text(
-                          'Video $index',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              const SizedBox(height: 50), // Espacio para dejar lugar a la lista horizontal
-              
-              Container(
-                height: 100,
-                color: Colors.grey,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 100,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      width: 100,
-                      margin: EdgeInsets.all(8),
-                      color: Colors.red,
-                      child: Center(
-                        child: Text(
-                          'Video $index',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              SizedBox(height: 50), // Espacio para dejar lugar a la lista horizontal
-              
-              Container(
-                height: 100,
-                color: Colors.grey,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 100,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      width: 100,
-                      margin: EdgeInsets.all(8),
-                      color: Colors.blue,
-                      child: Center(
-                        child: Text(
-                          'Video $index',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-
-              SizedBox(height: 50), // Espacio para dejar lugar a la lista horizontal
-              Container(
-                height: 100,
-                color: Colors.grey,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 100,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      width: 100,
-                      margin: EdgeInsets.all(8),
-                      color: Colors.red,
-                      child: Center(
-                        child: Text(
-                          'Video $index',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-
-              SizedBox(height: 50), // Espacio para dejar lugar a la lista horizontal
-              Container(
-                height: 100,
-                color: Colors.grey,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 100,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      width: 100,
-                      margin: EdgeInsets.all(8),
-                      color: Colors.blue,
-                      child: Center(
-                        child: Text(
-                          'Item $index',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              SizedBox(height: 50), // Espacio para dejar lugar a la lista horizontal
-              Container(
-                height: 100,
-                color: Colors.grey,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 100,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      width: 100,
-                      margin: EdgeInsets.all(8),
-                      color: Colors.red,
-                      child: Center(
-                        child: Text(
-                          'Item $index',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
+            ),
           ),
+
+          GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                childAspectRatio: 0.7,
+              ),
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset(
+                            "assets/images/logo.jpg",
+                            // popularShows[index].imageUrl,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "Titulo del curso",
+                        // popularShows[index].title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              })
         ],
       ),
-    );
-  }
-
-  Widget _buildCarousel(BuildContext context, int carouselIndex) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Text('Carousel $carouselIndex'),
-        SizedBox(
-          // you may want to use an aspect ratio here for tablet support
-          height: MediaQuery.of(context).size.height * .7,
-          child: PageView.builder(
-            // store this controller in a State to save the carousel scroll position
-            controller: PageController(viewportFraction: 0.8),
-            itemBuilder: (BuildContext context, int itemIndex) {
-              return _buildCarouselItem(context, carouselIndex, itemIndex);
-            },
-          ),
-        )
-      ],
-    );
-  }
-
-  Widget _buildCarouselItem(
-      BuildContext context, int carouselIndex, int itemIndex) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: Card(
-        elevation: 12,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20.0),
-        ),
-        color: Colors.black,
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topRight: Radius.circular(20),
-                topLeft: Radius.circular(20),
-              ),
-              child: Image.asset(
-                'assets/images/logo.jpg',
-                height: 200,
-                fit: BoxFit.cover,
-                width: double.infinity,
-              ),
-            ),
-            const ListTile(
-              title: Text(
-                'Melbourne Cricket Stadium',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              subtitle: Text(
-                'Australia',
-                style: TextStyle(
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-            Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextButton(
-                        style:
-                            TextButton.styleFrom(backgroundColor: Colors.white),
-                        onPressed: () {},
-                        child: const Text(
-                          "Edit",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Expanded(
-                      child: TextButton(
-                        style:
-                            TextButton.styleFrom(backgroundColor: Colors.red),
-                        onPressed: () {},
-                        child: const Text(
-                          "Delete",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ))
-          ],
-        ),
-      ),
-
-      // Container(
-      //   decoration: const BoxDecoration(
-      //     color: Colors.grey,
-      //     borderRadius: BorderRadius.all(Radius.circular(4.0)),
-      //   ),
-      // ),
     );
   }
 }
--- lib/pages/videos_pendientes/videos_pendientes_page.dart
+++ lib/pages/videos_pendientes/videos_pendientes_page.dart
@@ -8,58 +8,115 @@ class VideosPendientesPage extends StatefulWidget {
 }
 
 class _VideosPendientesPage extends State<VideosPendientesPage> {
-  @override
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
+      extendBodyBehindAppBar: false,
+      extendBody: true,
       appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.menu),
+        title: Image.asset(
+          'assets/images/logo_aifcf.png',
+          fit: BoxFit.contain,
+          height: 60,
         ),
-        title: Image.asset('assets/images/logo_aifcf.png', fit: BoxFit.contain, height: 60,),
         backgroundColor: Colors.transparent,
         elevation: 0,
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+        // actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
       ),
-      body: ListView.builder(
-        itemCount: 1,
-        itemBuilder: (BuildContext context, int index) {
-          return _buildCarousel(context, index ~/ 2);
-        }
-      ),
+      body: _body(),
     );
   }
-}
 
-  Widget _buildCarousel(BuildContext context, int carouselIndex) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Text('Videos sin empezar $carouselIndex'),
-        SizedBox(
-          // you may want to use an aspect ratio here for tablet support
-          height: 400,
-          child: PageView.builder(
-            // store this controller in a State to save the carousel scroll position
-            controller: PageController(viewportFraction: 0.8),
-            itemBuilder: (BuildContext context, int itemIndex) {
-              return _buildCarouselItem(context, carouselIndex, itemIndex);
-            },
+  Widget _body() {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              "Concluidos",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-        )
-      ],
-    );
-  }
+          // Horizontal list of featured content
+          Container(
+            height: 200,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.all(8),
+                  width: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/logo.jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
 
-  Widget _buildCarouselItem(BuildContext context, int carouselIndex, int itemIndex) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 4.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.grey,
-          borderRadius: BorderRadius.all(Radius.circular(4.0)),
-        ),
+          // Popular shows section
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              'Sin Empezar',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                childAspectRatio: 0.7,
+              ),
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset(
+                            "assets/images/logo.jpg",
+                            // popularShows[index].imageUrl,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "Titulo del curso",
+                        // popularShows[index].title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              })
+        ],
       ),
     );
   }
+
+}
